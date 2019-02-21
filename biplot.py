5b6fe888142ca2a8eaff7305bc8452ffdb109ac5
Serafeim Loukas <25902670+seralouk@users.noreply.github.com>
2019-02-21 22:19:28 +0100

    Update biplot.py

--- biplot.py
+++ biplot.py
@@ -17,6 +17,17 @@ pca = PCA()
 x_new = pca.fit_transform(X)
 
 def myplot(score,coeff,labels=None):
+    """
+    Author: Serafeim Loukas, EPFL, serafeim.loukas(at)epfl.ch, Last modified: 10/2017
+    Input
+    ------
+    score: the scores (projected data onto the forst 2 components)
+    coeff: the loadings (eigenvectors)
+    
+    Output
+    ------
+    plotting of the biplot
+    """
     xs = score[:,0]
     ys = score[:,1]
     n = coeff.shape[0]
